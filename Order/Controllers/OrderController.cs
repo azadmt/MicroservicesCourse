e501160f2dd0cec93ee29ac0cf7665c92b691366
Mohammad Azad <m.azad@live.com>
2021-08-20 03:30:02 +0430

    add dtc link

---
+++ Order/Controllers/OrderController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Order.Controllers
+{
+    public class OrderController : Controller
+    {
+        //https://www.c-sharpcorner.com/article/distributed-transactions-with-webapi-across-application-domains/
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
